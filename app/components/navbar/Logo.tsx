5d122ff117d20c00f9d608a2cefdca3199b445a8
unknown <amienulrana@gmail.com>
2023-12-24 21:33:32 +0700

    change logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,20 +1,26 @@
 'use client';
 
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { FaHotel } from "react-icons/fa";
 
 const Logo = () => {
   const router = useRouter();
 
   return ( 
-    <Image
-      onClick={() => router.push('/')}
-      className="hidden md:block cursor-pointer" 
-      src="/images/logo.png" 
-      height="100" 
-      width="100" 
-      alt="Logo" 
-    />
+    // <Image
+    //   onClick={() => router.push('/')}
+    //   className="hidden md:block cursor-pointer" 
+    //   src="/images/logo.png" 
+    //   height="100" 
+    //   width="100" 
+    //   alt="Logo" 
+    // />
+    <Link href={'/'}  className="text-primary hidden md:flex cursor-pointer gap-2 items-center">
+      <FaHotel className="text-2xl" />
+      <p className="text-lg font-bold">Nginep</p>
+    </Link>
    );
 }
  
